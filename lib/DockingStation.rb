4faa766cbf2aefb60e0fab5cd9ad0a9a89efb7c1
Khie Biggs <khiebigzy@gmail.com>
2018-05-15 17:36:14 +0100

    refactor

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,29 +1,34 @@
  require_relative 'Bike.rb'
 class DockingStation
   attr_reader :bikes
+
+  #put 20 bikes on the rack
   def initialize
     @bikes = []
     20.times do
       @bikes << Bike.new
     end
   end
+
+  #take a bike off
   def release_bike
     raise "no bike to be released" if empty?
     return @bikes.pop
   end
 
+  #dock a bike
   def dock_bike(bike)
     raise "can't have 20 bikes" if full?
     @bikes << bike
   end
 
   private
-
+  #check if full
   def full?
-    return true if @bikes.length == 20
-    return false
+    return @bikes.length == 20
   end
 
+  #check if empty
   def empty?
     return @bikes.empty?
   end
