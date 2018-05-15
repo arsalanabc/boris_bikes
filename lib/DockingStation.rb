9e854c38f2c9b01de962bda0bd9a4c44be63d432
Khie Biggs <khiebigzy@gmail.com>
2018-05-15 17:08:04 +0100

    Finish 12

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -3,7 +3,7 @@ class DockingStation
   attr_reader :bike
   def initialize
     @bike = Bike.new
-  end 
+  end
   def release_bike
     bikehold = @bike
     raise "no bike to be released" unless @bike
@@ -13,6 +13,7 @@ class DockingStation
   end
 
   def dock_bike(bike)
+    raise "can't have 2 bikes" if @bike
     @bike = bike
   end
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,12 +8,16 @@ describe DockingStation do
     expect(bike.working?).to eq(true) #eq(true)
   end
   it "allows me to dock a bike" do
+    subject.release_bike
     is_expected.to respond_to(:dock_bike)
     subject.dock_bike(Bike.new)
     expect(subject.bike).to be_a(Bike)
   end
   it "returns an error when trying to get bike if empty" do
     subject.release_bike
-    expect{subject.release_bike}.to raise_error
+    expect{subject.release_bike}.to raise_error("no bike to be released")
+  end
+  it "returns an error when trying to add a bike if one already exists" do
+    expect{subject.dock_bike(Bike.new)}.to raise_error("can't have 2 bikes")
   end
 end
