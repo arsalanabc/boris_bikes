984116a959a1cefdb4c5ac5f370dc4bb2e3acb32
Azlan <azlan@Azlans-MacBook-Pro.local>
2018-05-17 10:21:35 +0100

    Challenge 20 done - replace all the bike class instances with mocks

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,12 @@
 require 'DockingStation'
 describe DockingStation do
+
   it "Can release a bike" do
-    is_expected.to respond_to(:release_bike)
+    ds_mock = double
+    allow(ds_mock).to receive(:release_bike).and_return(nil)
+    expect(ds_mock).to respond_to(:release_bike)
+    #is_expected.to respond_to(:release_bike)
+
   end
 
   # let(:bike) {double :bike}
@@ -13,16 +18,16 @@ describe DockingStation do
   # end
 
 
-  let(:bike_mock) {double :bike} # using double for bike
+  #let(:bike_mock) {double :bike} # using double for bike
   it "returns a working bike" do
+    bike_mock = double # same as let(:bike_mock) {double :bike}
     allow(bike_mock).to receive(:working?).and_return(true)
     expect(bike_mock.working?).to eq(true) #eq(true)
   end
 
   it "allows me to dock a bike" do
-    is_expected.to respond_to(:dock_bike)
-    subject.dock_bike(Bike.new)
-    expect(subject.bikes.last).to be_a(Bike)
+    expect(subject).to respond_to(:dock_bike) #is_expected.to respond_to(:dock_bike)
+    expect(subject.bikes.last.class).to be_a(Bike.class)
   end
   it "returns an error when trying to get bike if empty" do
     expect{subject.release_bike}.to raise_error("no bike to be released")
@@ -39,8 +44,10 @@ describe DockingStation do
     expect(a.capacity).to eq 3
   end
   it "Should not release a broken bike" do
-    subject.dock_bike(Bike.new)
-    subject.bikes[-1].working = false
+    #mock_bike = double("Some Collaborator", :working? => false)
+    mock_bike = double
+    allow(mock_bike).to receive(:working?).and_return(false)
+    subject.dock_bike(mock_bike)
     expect{subject.release_bike}.to raise_error("Can't release broken bike")
   end
 end
